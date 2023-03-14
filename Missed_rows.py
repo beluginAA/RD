ee883f1d349f93f8b889d2b31da3abc213108c93
Белугин Александр Александрович <AlABelugin@rusas.local>
2023-03-14 17:05:18 +0300

    14.03.2023

--- Missed_rows.py
+++ Missed_rows.py
@@ -17,6 +17,9 @@ from tkinter.filedialog import askopenfilename  # Module for open file with win 
 # Ignoring pandas version errors
 warnings.simplefilter(action='ignore', category=(FutureWarning, UserWarning))
 
+# Event for new thread
+event = threading.Event()
+
 # show an "Open" dialog box and return the path to the selected file
 flag = False
 filename_comp = askopenfilename(title="Select file for compare", filetypes=[("Excel Files", "*.xlsx"), ("Excel Binary Workbook", "*.xlsb")])
@@ -31,55 +34,9 @@ with pyodbc.connect(conn_str) as conn:
     query = f'''SELECT * FROM {table}'''
     new_df = pd.read_sql(query, conn)
 
-# Function for changing code 
-def changing_code(df):
-    date_expected, date_release = df['Код KKS документа'], df['Код KKS документа_new']
-    if  not pd.isna(date_expected) and not pd.isna(date_release):
-        return f'Смена кода с <{date_expected}> на <{date_release}>'
-    else:
-        return date_expected
-
-# Function for changing name
-def changing_name(df):
-    date_expected, date_release = df['Наименование объекта/комплекта РД'], df['Наименование объекта/комплекта РД_new']
-    if  not pd.isna(date_expected) and not pd.isna(date_release) and date_expected != date_release:
-        return f'Смена наименования с <{date_expected}> на <{date_release}>'
-    else:
-        return date_expected
-
-# Function for converting date
-def convert_date(row):
-    if row in ['в производстве', 'В производстве', None]:
-        return row
-    else:
-        if dayfirst is True:
-            return pd.to_datetime(row, dayfirst=dayfirst).date()
-        else: 
-            return pd.to_datetime(row, format='%Y/%m/%d').date()
-
-# Function for changing_developer
-def change_developer(df):
-    if ~pd.isna(df['Разработчики РД (актуальные)']):
-        return df['Разработчик РД']
-    else:
-        return df['Разработчики РД (актуальные)']
-
-# Function for changing status
-def change_status(df):
-    if ~pd.isna(df['Статус текущей ревизии_new']):
-        return df['Статус РД в 1С']
-    else:
-        return df['Статус текущей ревизии_new']
-
-#Function for changing data
-def changing_data(row):
-    if (row[col] == row[f'{col}_new']) or (pd.isna(row[col]) and pd.isna(row[f'{col}_new'])):
-        return None
-    else:
-        return f'Смена {col.lower()} с <{row[col]}> на <{row[f"{col}_new"]}>'
+start_time = time.time()
 
 #Columns for changing dataframe
-
 convert_columns = ['Дата выпуска РД по договору подрядчика', 
                    'Дата выпуска РД по графику с Заказчиком', 
                    'Дата статуса Заказчика', 
@@ -141,6 +98,167 @@ base_columns = ['Система',
                 'КС',	
                 'Примечания']
 
+# Function for result-file
+def new_threading(df_1, df_2):
+
+    def change(df):
+        if ~pd.isna(df['Статус текущей ревизии']):
+            return df['Статус РД в 1С']
+        else:
+            return df['Статус текущей ревизии']
+
+    #  Preparation columns list with necessary information
+    mdf1_columns = ['Система',
+                'Наименование объекта/комплекта РД_new',
+                'Коды работ по выпуску РД',
+                'Тип',
+                'Пакет РД_new',	'Код KKS документа',
+                'Статус Заказчика_new',
+                'Текущая ревизия_new',
+                'Статус текущей ревизии_new',
+                'Дата выпуска РД по договору подрядчика_new',
+                'Дата выпуска РД по графику с Заказчиком_new',
+                'Дата статуса Заказчика_new',	
+                'Ожидаемая дата выдачи РД в производство_new',	
+                'Письма_new',	
+                'Источник информации_new',	
+                'Разработчики РД (актуальные)_new',	
+                'Объект_new',	
+                'WBS_new',	
+                'КС',	
+                'Примечания',
+                'Статус РД в 1С']
+    
+    mdf2_columns = ['Система',
+                'Наименование объекта/комплекта РД',
+                'Коды работ по выпуску РД',
+                'Тип',
+                'Пакет РД_new',	'Код KKS документа_new',
+                'Статус Заказчика_new',
+                'Текущая ревизия_new',
+                'Статус текущей ревизии_new',
+                'Дата выпуска РД по договору подрядчика_new',
+                'Дата выпуска РД по графику с Заказчиком_new',
+                'Дата статуса Заказчика_new',	
+                'Ожидаемая дата выдачи РД в производство_new',	
+                'Письма_new',	
+                'Источник информации_new',	
+                'Разработчики РД (актуальные)_new',	
+                'Объект_new',	
+                'WBS_new',	
+                'КС',	
+                'Примечания',
+                'Статус РД в 1С']
+    
+    new_columns = ['Система',
+                'Наименование объекта/комплекта РД',
+                'Коды работ по выпуску РД',
+                'Тип',
+                'Пакет РД',	'Код KKS документа',
+                'Статус Заказчика',
+                'Текущая ревизия',
+                'Статус текущей ревизии',
+                'Дата выпуска РД по договору подрядчика',
+                'Дата выпуска РД по графику с Заказчиком',
+                'Дата статуса Заказчика',	
+                'Ожидаемая дата выдачи РД в производство',	
+                'Письма',	
+                'Источник информации',	
+                'Разработчики РД (актуальные)',	
+                'Объект',	
+                'WBS',	
+                'КС',	
+                'Примечания',
+                'Статус РД в 1С']
+
+    #  Generate temporary dataframe for next appending for changed documents
+    print('Initiate dataframe with changed documents')
+    tmp_df = df_1[df_1['_merge'] == 'both'][mdf1_columns]
+    tmp_df.columns = new_columns
+
+    
+    # Generate result dataframe, with remove document firstly
+    print('Generating result dataframe, with remove document firstly')
+    changed_df = df_2[df_2['_merge'] == 'left_only'][new_columns]
+    tmp_df = pd.concat([changed_df, tmp_df])
+
+    #  Generate result dataframe 
+    print('Generating result dataframe ')
+    changed_df = df_2[df_2['_merge'] == 'both'][mdf2_columns]
+    changed_df.columns = new_columns
+    changed_df = pd.concat([changed_df, tmp_df])
+
+    # Changing dataframe
+    print('Changing dataframe')
+    changed_df['Дата выпуска РД по договору подрядчика'] = pd.to_datetime(changed_df['Дата выпуска РД по договору подрядчика'], dayfirst=True).dt.date
+    changed_df['Дата выпуска РД по графику с Заказчиком'] = pd.to_datetime(changed_df['Дата выпуска РД по графику с Заказчиком'], dayfirst=True).dt.date
+    changed_df['Дата статуса Заказчика'] = pd.to_datetime(changed_df['Дата статуса Заказчика'], dayfirst=True).dt.date
+    changed_df['Ожидаемая дата выдачи РД в производство'] = changed_df['Ожидаемая дата выдачи РД в производство'].apply(lambda row: row if row in ['в производстве', 'В производстве', None]  else pd.to_datetime(row).date())
+    changed_df['Объект'] = changed_df['Объект'].apply(lambda row: changed_df['Коды работ по выпуску РД'].str.slice(0, 5) if pd.isna(row) else row)
+    changed_df['Статус текущей ревизии'] = changed_df.apply(change, axis = 1)
+    changed_df['WBS'] = changed_df['WBS'].apply(lambda row: row if ~pd.isna(row) else changed_df['Коды работ по выпуску РД'].apply(lambda row: row[6 : row.find('.', 6)]))
+
+    event.wait()
+    comf_ren = input('Use standard file name (y/n): ')
+    while comf_ren not in 'YyNn':
+        comf_ren = input('For next work choose <y> or <n> simbols): ')
+
+    if comf_ren in 'Yy':
+        output_filename = 'result' + str(datetime.now().isoformat(timespec='minutes')).replace(':', '_')
+    else:
+        output_filename = input('Input result file name: ')
+    changed_df.to_excel(f'./{output_filename}.xlsx', encoding='cp1251', index = False)
+
+    end_time = time.time()
+    print(end_time - start_time)
+
+# Function for changing code 
+def changing_code(df):
+    date_expected, date_release = df['Код KKS документа'], df['Код KKS документа_new']
+    if  not pd.isna(date_expected) and not pd.isna(date_release):
+        return f'Смена кода с <{date_expected}> на <{date_release}>'
+    else:
+        return date_expected
+
+# Function for changing name
+def changing_name(df):
+    date_expected, date_release = df['Наименование объекта/комплекта РД'], df['Наименование объекта/комплекта РД_new']
+    if  not pd.isna(date_expected) and not pd.isna(date_release) and date_expected != date_release:
+        return date_release
+    else:
+        return date_expected
+
+# Function for converting date
+def convert_date(row):
+    if row in ['в производстве', 'В производстве', None]:
+        return row
+    else:
+        if dayfirst is True:
+            return pd.to_datetime(row, dayfirst=dayfirst).date()
+        else: 
+            return pd.to_datetime(row, format='%Y/%m/%d').date()
+
+# Function for changing_developer
+def change_developer(df):
+    if ~pd.isna(df['Разработчики РД (актуальные)']):
+        return df['Разработчик РД']
+    else:
+        return df['Разработчики РД (актуальные)']
+
+# Function for changing status
+def change_status(df):
+    if ~pd.isna(df['Статус текущей ревизии_new']):
+        return df['Статус РД в 1С']
+    else:
+        return df['Статус текущей ревизии_new']
+
+#Function for changing data
+def changing_data(row):
+    if (row[col] == row[f'{col}_new']) or (pd.isna(row[col]) and pd.isna(row[f'{col}_new'])):
+        return None
+    else:
+        return f'Смена {col.lower()} с <{row[col]}> на <{row[f"{col}_new"]}>'
+
 # read Excel files with current and new data
 print('Read excel files with current and new data')
 if '.xlsb' in filename_comp:
@@ -155,15 +273,17 @@ else:
     base_df = pd.read_excel(filename_comp)
 new_df.columns = base_columns
 
-#  Clear the data in both dataframe
-print('Clear the empty rows in both dataframes')
+#  Clearing dataframes
+print('Clearing dataframes')
 base_df = base_df.dropna(subset=['Коды работ по выпуску РД'])
 base_df['Разработчики РД (актуальные)'] = base_df.apply(change_developer, axis = 1)
+base_df = base_df[~base_df['Коды работ по выпуску РД'].str.contains('.C.', regex=False)]
+base_df['Код KKS документа'] = base_df['Код KKS документа'].astype(str)
+base_df = base_df.loc[~base_df['Код KKS документа'].str.contains('\.KZ\.|\.EK\.|\.TZ\.|\.KM\.|\.GR\.')]
 
-# Removing unnecessary data
-print('Clear the unnecessary data in base dataframe')
-base_df = base_df.loc[~base_df['Коды работ по выпуску РД'].str.contains('.C.')]
-base_df = base_df.loc[~base_df['Код KKS документа'].isin(['.KZ.', '.EK.', '.TZ.', '.KM.', '.GR.'])]
+#  Missed rows
+missed_code = new_df[~new_df['Коды работ по выпуску РД'].str.lower().str.strip().isin(base_df['Коды работ по выпуску РД'].str.lower().str.strip())]
+missed_code = missed_code.reset_index()[['Система', 'Коды работ по выпуску РД', 'Наименование объекта/комплекта РД']]
 
 #  Making copy of original dataframes
 print('Making copy of original dataframes')
@@ -172,12 +292,6 @@ new_df_copy = new_df.copy()
 
 #  Merging two dataframes dddd
 print('Merging two dataframes')
-missed_code = (new_df.merge(base_df,
-                           how='left',
-                           on=['Коды работ по выпуску РД'],
-                           suffixes=['', '_new'], 
-                           indicator=True))
-
 m_df_1 = (pd.merge(new_df_copy, base_df_copy,
                            how='left',
                            on=['Коды работ по выпуску РД', 'Код KKS документа'],
@@ -191,18 +305,7 @@ m_df_2 = (tmp_df.merge(base_df_copy,
                            suffixes=['', '_new'],
                            indicator=True))
 
-# на данном этапе можно выполнить копирование и запуск второго потока
-
-m_df_1 = m_df_1[m_df_1['_merge'] == 'both']
-m_df_1['Наименование объекта/комплекта РД'] = m_df_1.apply(lambda row: changing_name(row), axis = 1)
-m_df_2 = m_df_2[m_df_2['_merge'] == 'both']
-m_df_2['Код KKS документа'] = m_df_2.apply(lambda row: changing_code(row), axis = 1)
-
-# Converting types
-print('Converting types')
-m_df_1['Статус текущей ревизии_new'] = m_df_1.apply(change_status, axis = 1)
-m_df_2['Статус текущей ревизии_new'] = m_df_2.apply(change_status, axis = 1)
-
+#Changing dates
 if flag:  
     for col in convert_columns:
         dayfirst = False
@@ -224,25 +327,38 @@ else:
             m_df_1[col] = m_df_1[col].apply(convert_date)
             m_df_2[col] = m_df_2[col].apply(convert_date)
 
+#  Making copy of merging dataframes
+copy_df_1 = m_df_1.copy()
+copy_df_2 = m_df_2.copy()
+
+# Making a new thread fot result-file
+result_file = threading.Thread(target=new_threading, args = (copy_df_1, copy_df_2, ), name = 'result_file')
+result_file.start()
+
+# Changing dataframes
+print('Changing dataframes')
 for col in clmns:
     m_df_1[col] = m_df_1.apply(changing_data, axis = 1)
     m_df_2[col] = m_df_2.apply(changing_data, axis = 1)
 
+m_df_1 = m_df_1[m_df_1['_merge'] == 'both']
+m_df_1['Наименование объекта/комплекта РД'] = m_df_1.apply(lambda row: changing_name(row), axis = 1)
+m_df_2 = m_df_2[m_df_2['_merge'] == 'both']
+m_df_2['Код KKS документа'] = m_df_2.apply(lambda row: changing_code(row), axis = 1)
+m_df_1['Статус текущей ревизии_new'] = m_df_1.apply(change_status, axis = 1)
+m_df_2['Статус текущей ревизии_new'] = m_df_2.apply(change_status, axis = 1)
+
+# Preparing log-file
 m_df = pd.concat([m_df_1[changed_cols], m_df_2[changed_cols]])
 m_df = m_df.reset_index()[changed_cols]
-missed = missed_code[missed_code['_merge'] == 'left_only'].reset_index()[['Система', 'Коды работ по выпуску РД', 'Наименование объекта/комплекта РД']]
 max_len_row = [max(m_df[row].apply(lambda x: len(str(x)) if x else 0)) for row in m_df.columns]
 max_len_name = [len(row) for row in changed_cols]
 max_len = [col_len if col_len > row_len else row_len for col_len, row_len in zip(max_len_name, max_len_row)]
-
-comf_ren = input('Use standard file name for log file (y/n): ')
-while comf_ren not in 'YyNn':
-    comf_ren = input('For next work choose <y> or <n> simbols): ')
-
-if comf_ren in 'Yy':
-   output_filename = 'log-RD-' + str(datetime.now().isoformat(timespec='minutes')).replace(':', '_')
-else:
-    output_filename = input('Input result file name: ')
+cols = ['Система', 'Коды работ по выпуску РД', 'Наименование объекта/комплекта РД']
+max_missed_row = [max(missed_code[row].apply(lambda x: len(str(x)) if x else 0)) for row in  cols]
+max_missed_name = [len(row) for row in  cols]
+max_missed = [col_len if col_len > row_len else row_len for col_len, row_len in zip(max_missed_name,max_missed_row)]
+output_filename = 'log-RD-' + str(datetime.now().isoformat(timespec='minutes')).replace(':', '_')
 with open(f'{output_filename}.txt', 'w') as log_file:
     log_file.write('Список измененных значений:\n')
     log_file.write('\n')
@@ -258,41 +374,17 @@ with open(f'{output_filename}.txt', 'w') as log_file:
         log_file.write(f"{index: <{len(str(m_df.index.max()))}} | {column_value}\n")
     log_file.write('\n')
     log_file.write('Список отсутствующих кодов.\n')
-    log_file.write('     Коды работ по выпуску РД' + '\t | \t' + 'Наименование объекта/комплекта РД\n')
-    for index, row in missed.iterrows():
-        log_file.write(f'{str(index)}\t{row["Коды работ по выпуску РД"]}\t | \t{row["Наименование объекта/комплекта РД"]}\n')
+    log_file.write('\n')
+    file_write = ' ' * (len(str(m_df.index.max())) + 2)
+    for column, col_len in zip(cols, max_missed):
+        file_write += f"{column:<{col_len}}|"
+    log_file.write(file_write)
+    log_file.write('\n')
+    for index, row in missed_code.iterrows():
+        column_value = ''
+        for i in range(len(cols)):
+            column_value += f"{str(row[cols[i]]) if row[cols[i]] else '-':<{max_missed[i]}}|"
+        log_file.write(f"{index: <{len(str(missed_code.index.max()))}} | {column_value}\n")
 
-# Этот код полностью готов, принимает любые файлы и возвращает лог файл с измененными значениями
-# Подумать над реализацией многопоточности
-
-# import threading
-# import time
-
-# # Программа создает 10 потоков, доводит их до команды wait, проверяет условие, что количество потоков должно быть больше 10
-# # Далее ставит значение event как True и идет дальше
-
-# event = threading.Event() # На этом этапе значение автоматически устанавливается как False
-
-# def image_handler():
-#     thr_num = threading.currentThread().name
-#     print(f'Идет подготовка изображения из потока [{thr_num}]')
-#     event.wait()
-#     print(f'Изображение отправлено')
-
-# # def test():
-# #     while True:
-# #         event.wait() # Процесс продолжится только тогда, когда значение event = True
-# #         print('test')
-# #         time.sleep(2)
-
-# # event.clear() # Сброс значения event до значения False
-# for i in range(10):
-#     threading.Thread(target=image_handler, name = str(i)).start()
-#     print(f'Поток [{i}] запущен')
-#     time.sleep(1)
-
-# if threading.active_count() >= 10:
-#     event.set() # Задается значение True этой командой
-
-# time.sleep(10)
-# event.set()
+# Continue new thread
+event.set()
